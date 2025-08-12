8ef6153b608beb55255789cf81c84124073b7fb8
Luis <101060519+LuisFun@users.noreply.github.com>
2025-08-12 16:44:06 +0900

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeMathJax from 'rehype-mathjax';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [ mdx(), sitemap(), pagefind() ],
+	markdown: { remarkPlugins: [remarkMath], rehypePlugins: [rehypeMathJax] }
 });
