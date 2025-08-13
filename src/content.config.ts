26029451d24b7640310cfb050ad757f5484ae94c
Luis <101060519+LuisFun@users.noreply.github.com>
2025-08-13 14:51:23 +0900

    1

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,17 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
 
 const post = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/contents', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema:
-		z.object({
-			emoji: z.string(),
-			title: z.string(),
-			description: z.string(),
-			created: z.coerce.date(),
-			updated: z.coerce.date().optional(),
-		}),
-});
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: './src/contents', pattern: '**/*.{md,mdx}' }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    emoji: z.string(),
+    title: z.string(),
+    description: z.string(),
+    created: z.coerce.date(),
+    updated: z.coerce.date().optional(),
+  }),
+})
 
-export const collections = { post };
+export const collections = { post }
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,19 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { getCollection } from 'astro:content'
+import rss from '@astrojs/rss'
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts'
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const posts = await getCollection('post')
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map(post => ({
+      title: post.data.title,
+      pubDate: post.data.created,
+      description: post.data.description,
+      //customData: post.data.customData,
+      link: `/${post.id}/`,
+    })),
+  })
 }
