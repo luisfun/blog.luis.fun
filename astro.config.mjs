3073467383b2d372e9b00096ee2ebe3e45b5ed81
Luis <101060519+LuisFun@users.noreply.github.com>
2025-08-14 16:27:38 +0900

    fix: base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import remarkMath from 'remark-math';
-import rehypeMathJax from 'rehype-mathjax';
-import pagefind from "astro-pagefind";
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
+import icon from 'astro-icon'
+import pagefind from 'astro-pagefind'
+import rehypeMathJax from 'rehype-mathjax'
+import remarkMath from 'remark-math'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [ mdx(), sitemap(), pagefind() ],
-	markdown: { remarkPlugins: [remarkMath], rehypePlugins: [rehypeMathJax] }
-});
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), pagefind(), icon()],
+  markdown: { remarkPlugins: [remarkMath], rehypePlugins: [rehypeMathJax] },
+})
