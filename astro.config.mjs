3630ea37f331531600f4eaaa3984c82bb74f74e0
Luis <101060519+LuisFun@users.noreply.github.com>
2025-08-19 16:10:12 +0900

    feat: blog.luis.funへ公開

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,17 @@ import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 import icon from 'astro-icon'
 import pagefind from 'astro-pagefind'
+import rehypeExternalLinks from 'rehype-external-links'
 import rehypeMathJax from 'rehype-mathjax'
 import remarkMath from 'remark-math'
-import rehypeExternalLinks from "rehype-external-links"
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.luis.fun',
   trailingSlash: 'always',
   integrations: [mdx(), sitemap(), pagefind(), icon()],
-  markdown: { remarkPlugins: [remarkMath], rehypePlugins: [rehypeMathJax, [rehypeExternalLinks, { target: '_blank', rel: 'noreferrer' }]] },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathJax, [rehypeExternalLinks, { target: '_blank', rel: 'noreferrer' }]],
+  },
 })
